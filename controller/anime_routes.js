7502bd032d73aff10e43fc9956fe5e4d26c41012
DRG104 <ph_kyle104@yahoo.com>
2022-07-13 16:30:43 -0500

    fetch is working properly, issue with POST

--- controller/anime_routes.js
+++ controller/anime_routes.js
@@ -5,23 +5,28 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const router = express.Router()
 
 
-router.post('/show', (req, res) => {
-    const search = req.body.anime
-    const api = `https://api.jikan.moe/v4/anime?q=${search}&sfw`
-    fetch(api)
-    .then(res => res.json())
-    .then(data => {
-        res.render('anime/index', {data})
-    })
-    .catch(err => {
-        console.log("api")
-        res.json(err)
-    })
-})
+// router.post('/show', (req, res) => {
+//     const search = req.body.anime
+//     const api = `https://api.jikan.moe/v4/anime?q=${search}&sfw`
+//     fetch(api)
+//     .then(res => res.json())
+//     .then(data => {
+//         res.render('anime/index', {data})
+//     })
+//     .catch(err => {
+//         console.log("api")
+//         res.json(err)
+//     })
+// })
 
+
+// fetch(`https://api.jikan.moe/v4/anime?q=&sfw`)
+// .then(res => res.text())
+// .then(text => console.log(text))
 
 router.get('/', (req, res) => {
-    console.log("Getting index")
+    const search = req.body.anime
+    console.log(search)
     res.render('anime/index')
 })
 
