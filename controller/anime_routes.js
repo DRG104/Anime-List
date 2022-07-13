cce735f6082d27e29a6f6d25a1299e6edfacaa7b
DRG104 <ph_kyle104@yahoo.com>
2022-07-13 10:04:48 -0500

    trying to fetch api json

--- controller/anime_routes.js
+++ controller/anime_routes.js
@@ -3,6 +3,13 @@ const express = require('express')
 // making a router
 const router = express.Router()
 
+
+router.post('/show', (req, res) => {
+    const search = req.body.anime
+    const api = `https://api.jikan.moe/v4/anime?q=${anime}&sfw`
+})
+
+
 router.get('/', (req, res) => {
     res.render('anime/index')
 })
