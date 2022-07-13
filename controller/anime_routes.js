85f40d464146d66ec1d857b5d6dac55e21b9f9ca
DRG104 <ph_kyle104@yahoo.com>
2022-07-13 10:30:39 -0500

    trying to solve post issue

--- controller/anime_routes.js
+++ controller/anime_routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
 
 // making a router
 const router = express.Router()
@@ -6,7 +7,15 @@ const router = express.Router()
 
 router.post('/show', (req, res) => {
     const search = req.body.anime
-    const api = `https://api.jikan.moe/v4/anime?q=${anime}&sfw`
+    const api = `https://api.jikan.moe/v4/anime?q=${search}&sfw`
+    fetch(api)
+    .then(res => res.json())
+    .then(data => {
+        res.render('anime/index', {data})
+    })
+    .catch(err => {
+        res.json(err)
+    })
 })
 
 
