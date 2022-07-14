325e2126c6102a97392c0aef70c724c702ec729d
DRG104 <ph_kyle104@yahoo.com>
2022-07-14 09:37:26 -0500

    Fixed post issue

--- controller/anime_routes.js
+++ controller/anime_routes.js
@@ -4,29 +4,36 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 // making a router
 const router = express.Router()
 
-
 // router.post('/show', (req, res) => {
-//     const search = req.body.anime
-//     const api = `https://api.jikan.moe/v4/anime?q=${search}&sfw`
-//     fetch(api)
-//     .then(res => res.json())
-//     .then(data => {
-//         res.render('anime/index', {data})
-//     })
-//     .catch(err => {
-//         console.log("api")
-//         res.json(err)
-//     })
+//     res.render('anime/show')
 // })
 
+
+router.post('/show', (req, res) => {
+    const search = req.body.anime
+    const api = `https://api.jikan.moe/v4/anime?q=${search}&sfw`
+    fetch(api)
+    .then(res => res.json())
+    .then(data => {
+        res.render('anime/show', { data })
+        console.log(data)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+
+// anime by popularity 
+// https://api.jikan.moe/v4/top/anime/
 
 // fetch(`https://api.jikan.moe/v4/anime?q=&sfw`)
 // .then(res => res.text())
 // .then(text => console.log(text))
 
 router.get('/', (req, res) => {
-    const search = req.body.anime
-    console.log(search)
+    // const search = req.body
+    // console.log(search)
     res.render('anime/index')
 })
 
